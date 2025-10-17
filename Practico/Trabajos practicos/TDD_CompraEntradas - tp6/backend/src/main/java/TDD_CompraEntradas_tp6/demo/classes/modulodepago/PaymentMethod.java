135107affa36dd00f9ee7c079b0e934ef9adb66f
Mate04 <matusmaldonado@hotmail.com>
2025-10-17 19:42:35 -0300

    fix: arreglado conflicto de carpetas

--- Practico/Trabajos practicos/TDD_CompraEntradas - tp6/backend/src/main/java/TDD_CompraEntradas_tp6/demo/classes/modulodepago/PaymentMethod.java
+++
@@ -1,6 +0,0 @@
-package TDD_CompraEntradas_tp6.demo.classes.modulodepago;
-
-public enum PaymentMethod {
-    TARJETA,
-    EFECTIVO
-}
--- Practico/Trabajos practicos/TDD_CompraEntradas - tp6/backend/src/main/java/TDD_CompraEntradas_tp6/demo/classes/modulodepago/PaymentStatus.java
+++
@@ -1,7 +0,0 @@
-package TDD_CompraEntradas_tp6.demo.classes.modulodepago;
-
-public enum PaymentStatus {
-    REDIRECCION_MP,
-    PENDIENTE,
-    ERROR
-}
